07a77be0a6f80ba1945f96dac79c715d7d13dae6
Alex <alex@Alexs-MacBook-Air.fritz.box>
2025-05-18 21:15:56 +0200

    all pages with default style

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,15 @@ FROM python:3.11-slim
 # set working directory
 WORKDIR /app
 
+# copy requirements
+COPY requirements.txt requirements.txt
+
 # copy files
-COPY . /app
+COPY ./app .
 
 # install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# expose port
-EXPOSE 5000
-
 # run the Flask app
+ENV FLASK_APP=page:app
 CMD ["flask", "run", "--host=0.0.0.0"]
--- app/page.py
+++ app/page.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, send_from_directory
 from dotenv import load_dotenv
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="app/static")
 
 @app.route('/')
 def qrcode():
     return render_template('qrcode.html')
+
+@app.route('/empty')
+def empty():
+    return render_template('empty.html')
+
+@app.route('/readme')
+def readme():
+    return render_template('readme.html')
 
 @app.route('/index')
 def index():
@@ -30,6 +38,10 @@ def thankyou():
     email = request.args.get("email")
     avatar = request.args.get("avatar")
     return render_template('thankyou.html', user=user, email=email, avatar=avatar)
+
+@app.route("/static/<path:filename>")
+def serve_static(filename):
+    return send_from_directory("app/static", filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
